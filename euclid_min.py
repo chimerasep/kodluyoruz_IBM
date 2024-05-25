e229e48ac5bebf696ca8ea1972a12e89de688f08
Emre Can Şahin <b2210356146@cs.hacettepe.edu.tr>
2024-05-26 00:26:38 +0300

    Create euclid_min.py

---
+++ euclid_min.py
@@ -0,0 +1,16 @@
+import math
+
+points = [(1, 2), (4, 6), (5, 8), (9, 10)]
+
+def euclideanDistance(point1, point2):
+    return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
+
+distances = []
+
+for i in range(len(points)):
+    for j in range(i + 1, len(points)):
+        dist = euclideanDistance(points[i], points[j])
+        distances.append(dist)
+
+min_distance = min(distances)
+print("minimum mesafe:", min_distance)
